4c1abf887b4654f829e6405be480e20eecb19ab5
Ana Badolato <badolatoana@gmail.com>
2024-08-31 19:38:38 +0200

    primera Kata

---
+++ 7-kyu/multiples.js
@@ -0,0 +1,11 @@
+function multiple(x) {
+  if (x % 3 === 0 && x % 3 === 0) {
+    return "BangBoom";
+  } else if (x % 5 === 0) {
+    return "Boom";
+  } else if (x % 3 === 0) {
+    return "Bang";
+  } else {
+    return "Miss";
+  }
+}
